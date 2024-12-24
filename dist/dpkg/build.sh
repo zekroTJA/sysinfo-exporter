24570c133bd293e79a8684bd9c6c1cbd75bd51ef
zekroTJA <contact@zekro.de>
2024-12-24 10:12:45 +0100

    update dpkg build

--- dist/dpkg/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set -ex
-
-tag=$(git describe --tag --abbrev=0)
-
-sed 's/<<VERSION>>/'"${tag:1}"'/' -i DEBIAN/control
-
-dpkg-deb --build DEBIAN sysinfo-exporter.deb
---
+++ scripts/build-debian-package.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ -z "$1" ]; then
+    echo "usage: $(basename "$0") [arch]"
+    exit 1
+fi
+
+arch=$1
+tag=$(git describe --tag --abbrev=0)
+
+sed 's/<<VERSION>>/'"${tag:1}"'/' -i dist/dpkg/DEBIAN/control
+sed 's/<<ARCH>>/'"${arch}"'/' -i dist/dpkg/DEBIAN/control
+
+dpkg-deb --build dist/dpkg sysinfo-exporter-${tag}-${arch}.deb
