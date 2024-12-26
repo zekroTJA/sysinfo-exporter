eef660465c039ca94b16d4f52f2797e7447c676e
zekroTJA <contact@zekro.de>
2024-12-26 09:25:06 +0100

    update compression type for debian package

--- scripts/build-debian-package.sh
+++ scripts/build-debian-package.sh
@@ -11,4 +11,5 @@ tag=$(git describe --tag --abbrev=0)
 sed 's/<<VERSION>>/'"${tag:1}"'/' -i dist/dpkg/DEBIAN/control
 sed 's/<<ARCH>>/'"${arch}"'/' -i dist/dpkg/DEBIAN/control
 
-dpkg-deb --build dist/dpkg sysinfo-exporter-${tag}-${arch}.deb
+dpkg-deb -Zxz --build --root-owner-group \
+    dist/dpkg sysinfo-exporter-${tag}-${arch}.deb
